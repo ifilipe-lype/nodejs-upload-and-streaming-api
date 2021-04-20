774676293c1e7f22af418b5ab3452324fe32ff2b
ifilipe.lype <filipe7lype@gmail.com>
2021-04-20 18:56:50 +0100

    adds fileDownloadController

--- src/http/routes.js
+++ src/http/routes.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const uploadController = require("./controllers/fileUploadController");
 const streamController = require("./controllers/fileStreamController");
+const downloadController = require("./controllers/fileDownloadController");
 
 router.get("/stream/:filename", streamController);
+router.get("/download/:filename", downloadController);
 router.post("/upload", uploadController);
 
 module.exports = router;
